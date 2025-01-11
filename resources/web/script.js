1b0ce6c0bdeac0e3f228a21595715dc73afc7cb9
Chris Biagini <193523771+chris-biagini@users.noreply.github.com>
2025-01-11 18:18:16 -0500

    Added my version of saved state with expiration date.

--- resources/web/script.js
+++ resources/web/script.js
@@ -1,8 +1,9 @@
 class RecipeProgressManager {
 	constructor() {
 		// set properties
-		this.recipeId = document.body.dataset.recipeId || "defaultRecipe";
-		this.TTL = 24 * 60 * 60 * 1000; // 24 hours
+		this.recipeId = document.body.dataset.recipeId;
+		//this.storedStateTimeToLive = 48 * (60 * 60 * 1000); // 48 hours in ms
+		this.storedStateTimeToLive = 20 * 1000; // 20 seconds in ms
 
 		this.crossableItemNodes = document.querySelectorAll(
 			".ingredients li, .instructions p"
@@ -16,15 +17,67 @@ class RecipeProgressManager {
 
 	init() {
 		this.setupEventListeners();
+		this.loadRecipeState();
+	}
+
+	saveRecipeState() {
+		const currentRecipeState = {};
+		const currentCrossableItemState = {};
+
+		this.crossableItemNodes.forEach((crossableItemNode, index) => {
+			currentCrossableItemState[index] =
+				crossableItemNode.classList.contains("crossed-off");
+		});
+
+		currentRecipeState["crossableItemState"] = currentCrossableItemState;
+		currentRecipeState["lastInteractionTime"] = Date.now();
+
+		localStorage.setItem(
+			`saved-state-for-${this.recipeId}`,
+			JSON.stringify(currentRecipeState)
+		);
+	}
+
+	loadRecipeState() {
+		const storedRecipeState = JSON.parse(
+			localStorage.getItem(`saved-state-for-${this.recipeId}`)
+		);
+
+		if (!storedRecipeState) return;
+
+		const storedCrossableItemState =
+			storedRecipeState["crossableItemState"];
+
+		const storedLastInteractionTime =
+			storedRecipeState["lastInteractionTime"];
+
+		const stateAge = Date.now() - storedLastInteractionTime;
+
+		if (stateAge > this.storedStateTimeToLive) {
+			console.log(
+				"Saved state is too old (" +
+					stateAge +
+					" ms). Ignoring for now."
+			);
+			return;
+		}
+
+		this.crossableItemNodes.forEach((crossableItemNode, index) => {
+			if (storedCrossableItemState[index] === true) {
+				crossableItemNode.classList.add("crossed-off");
+			}
+		});
 	}
 
 	handleClickOnCrossableItem(crossableItemNode) {
 		crossableItemNode.classList.toggle("crossed-off");
-		console.log(crossableItemNode);
+		this.saveRecipeState();
 	}
 
 	handleClickOnSectionHighlighterItem(sectionHighlighterNode) {
-		sectionHighlighterNode.closest("section").classList.toggle("highlighted");
+		sectionHighlighterNode
+			.closest("section")
+			.classList.toggle("highlighted");
 	}
 
 	setupEventListeners() {
@@ -33,10 +86,12 @@ class RecipeProgressManager {
 				this.handleClickOnCrossableItem(crossableItemNode);
 			});
 		});
-		
+
 		this.sectionHighlighterNodes.forEach((sectionHighlighterNode) => {
 			sectionHighlighterNode.addEventListener("click", () => {
-				this.handleClickOnSectionHighlighterItem(sectionHighlighterNode);
+				this.handleClickOnSectionHighlighterItem(
+					sectionHighlighterNode
+				);
 			});
 		});
 	}
