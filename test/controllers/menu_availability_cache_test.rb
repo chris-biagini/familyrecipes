42499f2866217b3ae3ca20dabcc91ab910aa9fd4
Chris Biagini (sandbox) <193523771+chris-biagini@users.noreply.github.com>
2026-03-28 09:35:09 +0000

    Fix RuboCop offenses in menu availability cache test

--- test/controllers/menu_availability_cache_test.rb
+++ test/controllers/menu_availability_cache_test.rb
@@ -13,33 +13,36 @@ class MenuAvailabilityCacheTest < ActionDispatch::IntegrationTest
 
   test 'second menu load uses cached availability (fewer queries)' do
     get menu_path(kitchen_slug: kitchen_slug)
+
     assert_response :success
 
     first_count = count_queries { get menu_path(kitchen_slug: kitchen_slug) }
     second_count = count_queries { get menu_path(kitchen_slug: kitchen_slug) }
 
-    assert second_count <= first_count,
-           "Second load (#{second_count} queries) should not exceed first (#{first_count})"
+    assert_operator second_count, :<=, first_count,
+                    "Second load (#{second_count} queries) should not exceed first (#{first_count})"
   end
 
   test 'availability cache invalidates when kitchen is updated' do
     get menu_path(kitchen_slug: kitchen_slug)
+
     assert_response :success
 
     @kitchen.update_column(:updated_at, Time.current) # rubocop:disable Rails/SkipsModelValidations
 
     get menu_path(kitchen_slug: kitchen_slug)
+
     assert_response :success
   end
 
   private
 
-  def count_queries(&block)
+  def count_queries(&)
     count = 0
-    counter = ->(_name, _start, _finish, _id, payload) {
+    counter = lambda { |_name, _start, _finish, _id, payload|
       count += 1 unless payload[:name] == 'SCHEMA' || payload[:cached]
     }
-    ActiveSupport::Notifications.subscribed(counter, 'sql.active_record', &block)
+    ActiveSupport::Notifications.subscribed(counter, 'sql.active_record', &)
     count
   end
 end
